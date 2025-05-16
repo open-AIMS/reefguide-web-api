e20df85ca8afabd4d3512e70869f6fb961bea174
Peter Baker <peter.baker122@csiro.au>
2025-05-16 13:10:11 +1000

    Remove tODO
    Signed-off-by: Peter Baker <peter.baker122@csiro.au>

--- src/infra/infra.ts
+++ src/infra/infra.ts
@@ -178,7 +178,6 @@ export class ReefguideWebApiStack extends cdk.Stack {
           ],
           // This specifies the image to be used - should be in the full format
           // i.e. "ghcr.io/open-aims/reefguideapi.jl/reefguide-src:latest"
-          // TODO revert to latest
           workerImage: 'ghcr.io/open-aims/reefguideapi.jl/reefguide-src:latest',
           // TODO tinker with performance here - we can make these chunky if
           // needed as they should run transiently
